2f5d5126a7803d26876034039145c0b89a73de0a
Caio Roberto <caiorrs@gmail.com>
2022-04-23 13:34:05 -0300

    Arrays

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main() {
 	const conferenceTickets uint = 50
 	var remainingTickets uint = 50
 
+	// var bookings = [50]string{"Nana", "Nicole"}
+	// var bookings = [50]string{}
+	var bookings [50]string
+
 	// fmt.Printf("conferenceTickets is %T, remaining tickets is %T, conferenceName is %T\n\n", conferenceTickets, remainingTickets, conferenceName)
 
 	// fmt.Println("Welcome to the", conferenceName, "booking application")
@@ -23,10 +27,38 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
-	// ask the user for their name
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	// bookings[0] = "Nana"
+	// bookings[10] = "Nicole"
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask the user for their name - need to provide a pointer
+	fmt.Printf("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Printf("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Printf("Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Printf("Enter the number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	// fmt.Println(remainingTickets)
+	// fmt.Println(&remainingTickets)
+
+	// userName = "Tom"
+	// userTickets = 2
+	// fmt.Printf("User %v booked %v tickets\n", firstName, userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
