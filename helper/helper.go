ac4d88af4ef352ece875ae33a553691af6e4d3fc
Caio Roberto <caiorrs@gmail.com>
2022-04-23 21:27:29 -0300

    Packages

--- helper/helper.go
+++ helper/helper.go
@@ -1,8 +1,10 @@
-package main
+package helper
 
 import "strings"
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+// To export a function/variable/const..., need to capitalize first letter
+
+func ValidateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
 	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
